2efd4c50ee0a473b287de8e35219446746e7a6d8
Lao Zhuang <346255540@qq.com>
2012-04-16 22:39:26 +0800

    ff

---
+++ BowlingGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int score = 0;
+        private int[] rolls = new int[21];
+
+        private int currentRoll = 0;
+
+        public void roll(int pins)
+        {
+            score += pins;
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(frameIndex)) // strike
+                {
+                    score += 10 +
+                             rolls[frameIndex + 1] +
+                             rolls[frameIndex + 2];
+                    frameIndex++;
+                }else if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] +
+                         rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return score;
+        }
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+        private int sumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private int spareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int strikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] +
+                   rolls[frameIndex + 1] == 10;
+        }
+    }
+        
+}
---
+++ BowlingGameTest.cs
@@ -0,0 +1,95 @@
+﻿using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class BowlingGameTest 
+    {
+        private BowlingGame.Game g;
+
+        protected void setUp(){
+            g = new BowlingGame.Game();
+       }
+        private void rollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.roll(pins);
+        }
+
+        [TestMethod]
+        public void testGutterGame() {
+            //int n = 20;
+            //int pins = 0;
+            //BowlingGame.Game g = new BowlingGame.Game();
+            //for (int i = 0; i < n; i++)
+            //   g.roll(pins);     
+            try
+            {
+                rollMany(20, 0);
+                Assert.AreEqual(0, g.Score());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nMessage ---\n{0}", e.Message); ;
+            }
+      }
+       
+
+        [TestMethod]
+      public void testAllOnes()  {
+      //BowlingGame.Game g = new BowlingGame.Game();
+      //for (int i = 0; i < 20; i++)
+      //g.roll(1);
+          try
+          {
+              rollMany(20, 1);
+              Assert.AreEqual(20, g.Score());
+          }
+          catch (Exception e)
+          {
+              Console.WriteLine("\nMessage ---\n{0}", e.Message);
+          }
+  }
+        [TestMethod]
+     public void testOneSpare()  {
+         try
+         {
+             rollSpare();
+             g.roll(3);
+             rollMany(17, 0);
+             Assert.AreEqual(16, g.Score());
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine("\nMessage ---\n{0}", e.Message);
+         }
+  }
+     [TestMethod]
+     public void testOneStrike() {
+         try
+         {
+             rollStrike();
+             g.roll(3);
+             g.roll(4);
+             rollMany(16, 0);
+             Assert.AreEqual(24, g.Score());
+         }
+         catch (Exception e)
+         {
+             Console.WriteLine("\nMessage ---\n{0}", e.Message);
+         }       }
+     public void testPerfectGame() {            rollMany(12,10);
+            Assert.AreEqual(300, g.Score());      }
+     private void rollStrike()
+     {
+         g.roll(10);
+     }
+     private void rollSpare()
+        {
+            g.roll(5);
+            g.roll(5);
+        }    
+    }
+}
